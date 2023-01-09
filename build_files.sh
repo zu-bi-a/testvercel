e08f17df2924093d84ea71cc156e25b50e13f8d7
zu-bi-a <zubianasir29@gmail.com>
2023-01-09 19:17:10 +0530

    third

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo "BUILD START"
 
-python3.10.4 -m pip install -r requirements.txt
-python3.10.4 manage.py collectstatic --noinput --clear
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
 
